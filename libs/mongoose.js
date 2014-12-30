f9bfdf9064146f011b1391f2e76bb73e5896cf71
kate <lobanova.e.o@gmail.com>
2014-12-30 12:04:16 +0100

    MongoDB

---
+++ libs/mongoose.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/session');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('connection error:', err.message);
+});
+db.once('open', function callback () {
+    console.log("Connected to DB!");
+});
+
+var Schema = mongoose.Schema;
+
+var Users = new Schema({
+    role: {
+        type: String,
+        enum: ['professor', 'student'],
+        required: true
+    },
+    email: { type: String, required: true },
+	password: { type: Stinrg, required: true}
+});
+
+var UsersModel = mongoose.model('Users', Users);
+
+module.exports.UsersModel = UsersModel;
