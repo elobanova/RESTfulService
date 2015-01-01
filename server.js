02619b5cf02252a65e06a216387b52f0c731a3c7
kate <lobanova.e.o@gmail.com>
2015-01-01 21:59:31 +0100

    Added styles to pages

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({secret: 'mySecretKey'}));
 app.use(passport.initialize());
 app.use(passport.session());
